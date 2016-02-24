3abbb04db4237ae88bb58e850bf997796c5024c1
Matthew Slavin <mslavin@sapient.com>
2016-02-24 13:06:54 -0500

    adding messages to rebase via vommand line

--- main.js
+++ main.js
@@ -20,8 +20,15 @@ This is another change on main.js on the rebase branch
 
 
 -- 12.39  Updating main.js on rebase branch
+-- 12.58 testing rebase using command line
+
+
 
 feb 26 - -  9.55 adding another commit after rebase
 
 
 10:12 testing git commit v2 - added to main.js
+
+feb 26 - -  9.55 adding another commit after rebase
+
+-- 12.58 testing rebase using command line
